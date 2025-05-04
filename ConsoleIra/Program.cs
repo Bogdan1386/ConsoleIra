502ec5b6edd455d8302a514c1281b90611f30434
gotfr <gotfr@BOGDAN>
2025-05-04 15:26:51 +0300

    Add project files.

---
+++ ConsoleIra/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleIra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nala = 23;
+            int nala2 = 34;
+            string nala3 = "ira1";
+            int result1 = Sum(nala3,nala, nala2);
+            Console.WriteLine(result1);
+
+            int result2 = Sum("sum2result", 32, 54);
+            Console.WriteLine(result2);
+            
+        }
+
+        private static int Sum(string ira,int x, int y)
+        {
+            int din = x+y;
+            int don = 3;
+            don = din;
+            Console.WriteLine(don);
+            Console.WriteLine(ira);
+            return din;
+        }
+
+
+    }
+}
